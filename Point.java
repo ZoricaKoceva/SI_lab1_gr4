a88cc99949984ac7b96e494bb6f1dbad634aedd0
ZoricaKoceva <zorica.koceva@students.finki.ukim.mk>
2020-03-26 03:00:56 +0100

    class Canvas and methods added

--- Point.java
+++ Point.java
@@ -13,6 +13,11 @@ class Point {
 		this.dotColor = dotColor;
 	}
 
+	Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
 	//TODO setters and getters
 	public String getId() {
 		return id;
@@ -76,6 +81,63 @@ class Point {
 		System.out.println("( " + getX() + ", " + getY() + " )");
 	}
 
+}
+
+class Canvas extends Point{
+	List<Point> pointsList = new ArrayList<Point>();
+	int N; // number of points for example, as parametar in method findingNClosestPoints
+
+	public Canvas( List<Point> pointsList, int N, String id, double x, double y, String dotColor) {
+		super(id, x, y, dotColor);
+		this.pointsList = pointsList;
+		this.N = N;
+	}
+
+	public void addPoint(){
+		for (Point point : pointsList){
+			pointsList.add(point);
+		}
+	}
+
+	public void deletePoint(Point point){
+		if (pointsList.contains(point)){
+			pointsList.remove(point);
+		}
+	}
+
+	public void moveAllPoints(char xDirection, char yDirection){
+		for( Point point : pointsList) {
+			point.move(xDirection,yDirection);
+		}
+	}
+
+	public Point middlePointOfAllPoints(Point point){
+		double pointCoordinateX = (point.x + x)/2;
+		double pointCoordinateY = (point.y + y)/2;
+		return new Point(pointCoordinateX, pointCoordinateY);
+	}
+
+	public static double pointsDistance(double x1, double y1, double x2, double y2) {
+		x1 -= x2;
+		y1 -= y2;
+		return Math.sqrt(pow(x1 ,2) + pow(y1,2));
+	}
+
+	public void findingNClosestPoints(int N, Point toPoint, List<Point> pointsList){
+
+		for (toPoint : pointsList){
+			for (int i=0;i<N;i++){
+				Point closestPoint = pointsList.get(i);
+				double closestDistace = pointsDistance(toPoint.x, toPoint.y, pointsList.get(i).x, pointsList.get(i).y);
+				double distance = pointsDistance(x1, y1, x2, y2);
+
+				if (distance < closestDistace && distance != 0){
+					closestDistace = distance;
+					closestPoint = pointsList.get(i);
+				}
+			}
+		}
+	}
 }
 
 // public class Point{ //testing the functionality of class Point
